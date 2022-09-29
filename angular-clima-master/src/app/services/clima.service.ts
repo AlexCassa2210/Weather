064c4fe93b265f15cbeb3f0f1859222342ed9dca
Alex Cascante <60294578+AlexCassa2210@users.noreply.github.com>
2022-09-29 08:44:13 -0600

    Update clima.service.ts

--- angular-clima-master/src/app/services/clima.service.ts
+++ angular-clima-master/src/app/services/clima.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class ClimaService {
 
   url = 'http://api.openweathermap.org/data/2.5/weather?&appid=';
-  key = 'be665555ac9a1636a085b7a69869a89c';
+  key = 'My_Api_key';
 
   constructor(private http: HttpClient) { }
 
